c99d8effcf011692139ea007942dab54c0ddd732
Androz2091 <androz2091@gmail.com>
2023-06-27 11:35:50 +0200

    use null instead

--- src/app.py
+++ src/app.py
@@ -255,7 +255,7 @@ def get_avatar(package_id, user_id):
 
         return jsonify({
             'user_id': user_id,
-            'display_name': user['display_name'] if 'display_name' in user and user['display_name'] else user['username'],
+            'display_name': user['display_name'] if 'display_name' in user and user['display_name'] else None,
             'avatar_url': avatar_url
         }), 200
 
