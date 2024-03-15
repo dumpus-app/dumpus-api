8e1460fac67c707d2b9923c8035adf180860942c
Androz 2091 <androz2091@gmail.com>
2024-03-15 21:20:35 +0100

    add some logs in production

--- src/tasks.py
+++ src/tasks.py
@@ -75,8 +75,9 @@ def download_file(package_status_id, package_id, link, session):
     if not check_whitelisted_link(link):
         print('checking content type')
         r = requests.head(link, allow_redirects=True)
-        # check if location starts with https://storage.googleapis.com after 302
-        if r.status_code != 302 and not r.headers['Location'].startswith('https://storage.googleapis.com'):
+        print(r.status_code)
+        print(r.headers)
+        if r.status_code != 200 or 'content-type' not in r.headers or 'application/octet-stream' not in r.headers['content-type']:
             print('The link does not point to a valid file.')
             raise Exception('INVALID_LINK')
 
